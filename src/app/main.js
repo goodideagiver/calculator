be2fd7802a2420fce1f8cb379872081e8637e3d0
Karol <kariurek@gmail.com>
2022-01-28 12:36:26 +0100

    fixed multiply

--- src/app/main.js
+++ src/app/main.js
@@ -1,28 +1,21 @@
+const replaceOperators = (input) => {
+	let replaced = input.replace('x', '*');
+	replaced = replaced.replace(':', `/`);
+	return replaced;
+};
+
 const countExpression = () => {
 	const input = replaceOperators(screen.innerText);
 	console.log('input :', input);
 	const compiled = math.compile(input);
 	console.log('compiled :', compiled);
 	if (compiled) {
-		let result;
-		try {
-			result = compiled.evaluate();
-		} catch (error) {
-			alert('Input error');
-			clearScreen();
-			return;
-		}
+		const result = compiled.evaluate();
 		console.log('result :', result);
 		updateScreen(result.toFixed(2));
 	} else {
 		alert('Could not compile');
 	}
-};
-
-const replaceOperators = (input) => {
-	let replaced = input.replace('x', '*');
-	replaced = input.replace(':', `/`);
-	return replaced;
 };
 
 equals.addEventListener('click', countExpression);
